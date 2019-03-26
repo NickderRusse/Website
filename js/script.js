006113c88ca8cbba594b78fa6e688d2858f9b7ab
Mr Mister <35255372+NickderRusse@users.noreply.github.com>
2019-03-26 20:02:02 +0100

    Delete script.js

--- js/script.js
+++
@@ -1,9 +0,0 @@
-
-window.onload=function(){
-  c=document.getElementById('gc');
-  cc = c.getContext('2d');
-  setInterval(update,1000/30);
-}
-
-cc.fillStyle='black';
-cc.fillRect(0,0,c.width,c.height);
